d1c4cc5edc249a07dd9095f33b4a09fd39111c85
Confident-me <296388293@qq.com>
2016-03-20 10:53:04 +0800

    主页

---
+++ js/js.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function ($) {
+
+
+    //initialise Stellar.js
+    $(window).stellar();
+
+    //Cache some variables
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+    
+    slide.waypoint(function (event, direction) {
+        dataslide = $(this).attr('data-slide');
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    }); 
+
+
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
+    //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+
+
+});
